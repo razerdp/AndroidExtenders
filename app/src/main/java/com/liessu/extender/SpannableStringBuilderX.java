e6a707c849645e7cd53c8ab3e0b2fdcd354f66cc
LiesSu <285117876@qq.com>
2016-03-26 11:47:52 +0800

    1.新增appendImageSpan()方法，简化插入ImageSpan的代码量

--- app/src/main/java/com/liessu/extender/SpannableStringBuilderX.java
+++ app/src/main/java/com/liessu/extender/SpannableStringBuilderX.java
@@ -1,8 +1,11 @@
 package com.liessu.extender;
 
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.style.DynamicDrawableSpan;
+import android.text.style.ImageSpan;
 import android.util.Log;
 
 /**
@@ -35,6 +38,38 @@ public class SpannableStringBuilderX extends SpannableStringBuilder{
     }
 
 
+    public SpannableStringBuilderX append(char text) {
+        append(String.valueOf(text));
+        return this;
+    }
+
+    public SpannableStringBuilderX append(CharSequence text) {
+        int length = length();
+        replace(length, length, text, 0, text.length());
+        return this;
+    }
+
+    public SpannableStringBuilder append(CharSequence text, int start, int end) {
+        int length = length();
+        replace(length, length, text, start, end);
+        return this;
+    }
+
+
+    /**
+     * Appends the character sequence {@code text} and spans {@code what} over the appended part.
+     * See {@link Spanned} for an explanation of what the flags mean.
+     * @param text the character sequence to append.
+     * @param what the object to be spanned over the appended text.
+     * @param flags see {@link Spanned}.
+     * @return this {@code SpannableStringBuilder}.
+     */
+    public SpannableStringBuilderX append(CharSequence text, Object what, int flags) {
+            int start = length();
+            append(text);
+            setSpan(what, start, length(), flags);
+            return this;
+    }
 
     /**
      * Appends the character sequence {@code text} and spans {@code what} over the appended part.
@@ -56,20 +91,37 @@ public class SpannableStringBuilderX extends SpannableStringBuilder{
 
 
     /**
-     * Appends the character sequence {@code text} and spans {@code what} over the appended part.
-     * See {@link Spanned} for an explanation of what the flags mean.
-     * @param text the character sequence to append.
-     * @param what the object to be spanned over the appended text.
-     * @param flags see {@link Spanned}.
-     * @return this {@code SpannableStringBuilderX}.
+     * Append ImageSpan easily .
+     * @param drawable  drawable instance to insert
+     * @param imageSpanFlags one of {@link DynamicDrawableSpan#ALIGN_BOTTOM} or
+     * {@link DynamicDrawableSpan#ALIGN_BASELINE}.
+     * @param spanFlags determine how the span will behave
+     * @return SpannableStringBuilderX instance
      */
-    @Override
-    public SpannableStringBuilderX append(CharSequence text, Object what, int flags) {
-            int start = length();
-            append(text);
-            setSpan(what, start, length(), flags);
-            return this;
+    public SpannableStringBuilderX appendImageSpan(Drawable drawable , int imageSpanFlags , int spanFlags ){
+        int start = length();
+        append("ImageSpan");
+        setSpan(new ImageSpan(drawable , imageSpanFlags), start, length(), spanFlags);
+        return this;
     }
+
+    /**
+     *  Cat off  the SpannableStringBuilderX's content , then build new one .
+     * @param start cat off start index
+     * @param end cat off end index
+     * @return a new SpannableStringBuilderX instance .
+     */
+    public SpannableStringBuilderX rebuild(int start , int end){
+        return new SpannableStringBuilderX(this, start, end);
+    }
+
+    /**
+     * Return a CharSequence containing a copy of the chars in this buffer.
+     */
+    public CharSequence toSequence() {
+        return new SpannableStringBuilder(this);
+    }
+
 
 
     /**
@@ -83,7 +135,7 @@ public class SpannableStringBuilderX extends SpannableStringBuilder{
         if( getVersion() >= Build.VERSION_CODES.M)
             return super.getTextWatcherDepth();
         else {
-            Log.e( TAG , "Android sdk  version number must be greater than or equal to 23. ");
+            Log.e(TAG , "Android sdk  version number must be greater than or equal to 23. ");
             return -1;
         }
     }
