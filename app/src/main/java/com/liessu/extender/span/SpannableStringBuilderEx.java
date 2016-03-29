31eba2cf964077b579bb640ff32cfb39c79b72af
LiesSu <LiesSu@outlook.com>
2016-03-29 15:05:47 +0800

    1. 集成replace、insert、delete系列方法；2.新增replace()、replaceAll()，简化图片/表情替换指定字符串的操作

--- app/src/main/java/com/liessu/extender/span/SpannableStringBuilderEx.java
+++ app/src/main/java/com/liessu/extender/span/SpannableStringBuilderEx.java
@@ -1,7 +1,21 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.liessu.extender.span;
 
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.DynamicDrawableSpan;
@@ -12,7 +26,7 @@ import android.util.Log;
  *  A extensional class for SpannableStringBuilder , ignore the Android sdk version .
  */
 public class SpannableStringBuilderEx extends SpannableStringBuilder{
-    private final static String TAG = "SpannableStringBuilderEx";
+    private final static String TAG = "SpanStringBuilderEx";
 
     /**
      * Create a new SpannableStringBuilderEx with empty contents
@@ -38,17 +52,20 @@ public class SpannableStringBuilderEx extends SpannableStringBuilder{
     }
 
 
+    @Override
     public SpannableStringBuilderEx append(char text) {
         append(String.valueOf(text));
         return this;
     }
 
+    @Override
     public SpannableStringBuilderEx append(CharSequence text) {
         int length = length();
         replace(length, length, text, 0, text.length());
         return this;
     }
 
+    @Override
     public SpannableStringBuilder append(CharSequence text, int start, int end) {
         int length = length();
         replace(length, length, text, start, end);
@@ -64,6 +81,7 @@ public class SpannableStringBuilderEx extends SpannableStringBuilder{
      * @param flags see {@link Spanned}.
      * @return this {@code SpannableStringBuilder}.
      */
+    @Override
     public SpannableStringBuilderEx append(CharSequence text, Object what, int flags) {
             int start = length();
             append(text);
@@ -95,13 +113,86 @@ public class SpannableStringBuilderEx extends SpannableStringBuilder{
      * @param drawable  drawable instance to insert
      * @param imageSpanFlags one of {@link DynamicDrawableSpan#ALIGN_BOTTOM} or
      * {@link DynamicDrawableSpan#ALIGN_BASELINE}.
-     * @param spanFlags determine how the span will behave
+     * @param spanFlags determine how the span will behave. see {@link Spanned}.
      * @return SpannableStringBuilderEx instance
      */
     public SpannableStringBuilderEx appendImageSpan(Drawable drawable , int imageSpanFlags , int spanFlags ){
         int start = length();
         append("ImageSpan");
         setSpan(new ImageSpan(drawable , imageSpanFlags), start, length(), spanFlags);
+        return this;
+    }
+
+    @Override
+    public SpannableStringBuilderEx replace(int start, int end, CharSequence tb) {
+        replace(start, end, tb);
+        return this;
+    }
+
+    @Override
+    public SpannableStringBuilderEx replace(int start, int end, CharSequence tb, int tbstart, int tbend) {
+        replace(start, end, tb, tbstart, tbend);
+        return this;
+    }
+
+    /**
+     * Replace first matched string with ImageSpan.
+     * @param str  replaced string
+     * @param start replace start index
+     * @param drawable drawable replaces the matched string content .
+     * @param imgFlag image span flag . one of {@link DynamicDrawableSpan#ALIGN_BOTTOM} or
+     * {@link DynamicDrawableSpan#ALIGN_BASELINE}.
+     * @param spanFlag determine how the span will behave. see {@link Spanned}.
+     */
+    public SpannableStringBuilderEx replace(@NonNull String str , int start , @NonNull Drawable drawable ,int imgFlag , int spanFlag){
+        if(str == null || drawable == null){
+            Log.e(TAG , "NullPointException..");
+            return null;
+        }
+
+        int index = start + toString().substring(start).indexOf(str);
+        setSpan(new ImageSpan(drawable,imgFlag) , index ,index + str.length() , spanFlag);
+        return this;
+    }
+
+    /**
+     * Replace all matched string with ImageSpan.
+     * @param str  replaced string
+     * @param drawable drawable replaces the matched string content .
+     * @param imgFlag image span flag . one of {@link DynamicDrawableSpan#ALIGN_BOTTOM} or
+     * {@link DynamicDrawableSpan#ALIGN_BASELINE}.
+     * @param spanFlag determine how the span will behave. see {@link Spanned}.
+     */
+    public SpannableStringBuilderEx replaceAll(@NonNull String str , @NonNull Drawable drawable ,int imgFlag , int spanFlag) {
+        if(str == null || drawable == null){
+            Log.e(TAG , "NullPointException..");
+            return null;
+        }
+
+        String content = toString();
+        int index = 0;
+        while((index =content.indexOf(str,index)) != -1){
+            setSpan(new ImageSpan(drawable,imgFlag) , index ,index + str.length() , spanFlag);
+            index += str.length();
+        }
+        return this;
+    }
+
+    @Override
+    public SpannableStringBuilderEx insert(int where, CharSequence tb) {
+        insert(where, tb);
+        return this;
+    }
+
+    @Override
+    public SpannableStringBuilderEx insert(int where, CharSequence tb, int start, int end) {
+        insert(where, tb, start, end);
+        return this;
+    }
+
+    @Override
+    public SpannableStringBuilderEx delete(int start, int end) {
+        delete(start, end);
         return this;
     }
 
@@ -136,7 +227,7 @@ public class SpannableStringBuilderEx extends SpannableStringBuilder{
         if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             return super.getTextWatcherDepth();
         else {
-            Log.e(TAG , "Android sdk  version number must be greater than or equal to 23. ");
+            Log.e(TAG , "Android sdk version number must be greater than or equal to 23. ");
             return -1;
         }
     }
